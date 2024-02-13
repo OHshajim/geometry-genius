2ba6e76d913817b44f549fbbb613490078bf7191
OHshajim <ajshajimmax@gmail.com>
2024-02-13 06:51:28 +0600

    add rectangle and calculate the area

--- script/rectangle.js
+++ script/rectangle.js
@@ -1,12 +1,30 @@
-/** to get rectangle area 
- *   1. get  base value 
- *   2. get hight value 
+/** to get rectangle area  manually
+ *   1. get  width value 
+ *   2. get length value 
  *   3. value type convert to float from string
  *   4. calculate area by using formula 
  *   5. return the value
  */
 
 // a function for area 
-function RectangleAreaCalculate(){
+function RectangleAreaCalculate() {
+    // get  width value 
+    const rectangleWidth = document.getElementById('rectangle-width');
+    const WidthValue = rectangleWidth.value;
+    const width = parseFloat(WidthValue);
+    console.log(width);
+
+    // get length value 
+    const rectangleLength = document.getElementById('rectangle-length');
+    const lengthValue = rectangleLength.value;
+    const length = parseFloat(lengthValue);
+    console.log(length);
+    
+    // calculate area by using formula
+    const area = width * length;
+
+    // return the value
+    const areaValue = document.getElementById('rectangle-area');
+    areaValue.innerText = area;
 
 }
