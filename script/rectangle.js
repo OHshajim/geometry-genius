286207d82324738a6635b1f12881459fe9d829fd
OHshajim <ajshajimmax@gmail.com>
2024-02-13 06:29:30 +0600

    area of triangle

---
+++ script/rectangle.js
@@ -0,0 +1,12 @@
+/** to get rectangle area 
+ *   1. get  base value 
+ *   2. get hight value 
+ *   3. value type convert to float from string
+ *   4. calculate area by using formula 
+ *   5. return the value
+ */
+
+// a function for area 
+function RectangleAreaCalculate(){
+
+}
--- script/triangle.js
+++ script/triangle.js
@@ -3,23 +3,28 @@
  * 2. get hight value 
  * 3. value type convert to float from string
  * 4. calculate area by using formula 
- * 
- * 
- * 
+ * 5. return the value
  */
 function TriangleAreaCalculate() {
     // input the base value 
     const triangleBase = document.getElementById('triangle-base');
     const BaseValue = triangleBase.value;
     const base = parseFloat(BaseValue);
-    console.log(base)
+    // console.log(base);
 
     // input the hight value 
     const triangleHight = document.getElementById('triangle-hight');
-    const HightValue = triangleHight.value
-    const hight = parseFloat(HightValue)
-    console.log(hight);
+    const HightValue = triangleHight.value;
+    const hight = parseFloat(HightValue);
+    // console.log(hight);
  
+    // calculate area 
+    // formula : 0.5 X base X hight 
+    const area = 0.5 * base * hight ;
+    // console.log(area);
 
+    // Return the area value 
+    const areaValue = document.getElementById('triangle-area');
+    areaValue.innerText = area ;
 
 };
